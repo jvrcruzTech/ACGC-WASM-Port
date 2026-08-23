0915ad52ae8bd05c07843ceacedf61bc1ac0c358
jvrcruzGAMES <69850408+jvrcruzGAMES@users.noreply.github.com>
2026-08-23 22:15:56 +0000

    cc

--- pc/src/pc_pad.c
+++ pc/src/pc_pad.c
@@ -73,7 +73,6 @@ void pc_web_set_pad_state(u16 buttons, s8 stickX, s8 stickY, s8 substickX, s8 su
 #endif
 
 BOOL PADInit(void) {
-#ifndef __EMSCRIPTEN__
     for (int i = 0; i < SDL_NumJoysticks(); i++) {
         if (SDL_IsGameController(i)) {
             g_controller = SDL_GameControllerOpen(i);
@@ -82,14 +81,14 @@ BOOL PADInit(void) {
             }
         }
     }
-#endif
     return TRUE;
 }
 
-#ifdef __EMSCRIPTEN__
 u32 PADRead(PADStatus* status) {
     memset(status, 0, sizeof(PADStatus) * 4);
 
+#ifdef __EMSCRIPTEN__
+    /* Block BOTH SDL and Web JS inputs together at once when pad is frozen by Web UI */
     if (s_web_pad_frozen || (g_pc_typing_mode && g_pc_editor_active)) {
         status[0].button = 0;
         status[0].stickX = 0;
@@ -101,21 +100,7 @@ u32 PADRead(PADStatus* status) {
         status[0].err = 0;
         return PAD_CHAN0_BIT;
     }
-
-    status[0].button = s_web_pad_buttons;
-    status[0].stickX = s_web_stick_x;
-    status[0].stickY = s_web_stick_y;
-    status[0].substickX = s_web_substick_x;
-    status[0].substickY = s_web_substick_y;
-    status[0].triggerLeft = s_web_trigger_l;
-    status[0].triggerRight = s_web_trigger_r;
-    status[0].err = 0;
-
-    return PAD_CHAN0_BIT;
-}
-#else
-u32 PADRead(PADStatus* status) {
-    memset(status, 0, sizeof(PADStatus) * 4);
+#endif
 
     const u8* keys = SDL_GetKeyboardState(NULL);
     u32 mouse = SDL_GetMouseState(NULL, NULL);
@@ -228,6 +213,17 @@ u32 PADRead(PADStatus* status) {
         triggerR = pad_trigger_value(pb->r);
     }
 
+#ifdef __EMSCRIPTEN__
+    /* Combine Web JS pad inputs together at once with SDL inputs */
+    buttons |= s_web_pad_buttons;
+    if (s_web_stick_x != 0) stickX = s_web_stick_x;
+    if (s_web_stick_y != 0) stickY = s_web_stick_y;
+    if (s_web_substick_x != 0) cstickX = s_web_substick_x;
+    if (s_web_substick_y != 0) cstickY = s_web_substick_y;
+    if (s_web_trigger_l != 0) triggerL = s_web_trigger_l;
+    if (s_web_trigger_r != 0) triggerR = s_web_trigger_r;
+#endif
+
     status[0].button = buttons;
     status[0].stickX = stickX;
     status[0].stickY = stickY;
@@ -239,7 +235,6 @@ u32 PADRead(PADStatus* status) {
 
     return PAD_CHAN0_BIT; /* Controller 1 connected */
 }
-#endif
 
 void PADControlMotor(s32 chan, u32 command) {
     if (g_controller && chan == 0) {
